5847b900a7f94ed8b09dda04960f074e07e9fdad
Hazar <92859817+HazarBakir@users.noreply.github.com>
2025-02-05 15:34:08 +0300

    refactor: fix unefficient code in PlayerController.cs

--- SkyBoundQuest/Assets/Scripts/PlayerConroller.cs
+++ SkyBoundQuest/Assets/Scripts/PlayerConroller.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -6,83 +8,129 @@ public class PlayerController : MonoBehaviour
     public float fallMultiplier = 2.5f;
     public float lowJumpMultiplier = 2f;
     public float jumpVelocity;
+
     [SerializeField] private float moveSpeed = 5f;
-    private float moveInput;
-    private Rigidbody2D rb;
+    private float _moveInput;
+    private Rigidbody2D _rb;
     public float currentSpeed;
-    
+
     [SerializeField] private Transform groundCheck;
+    [SerializeField] private Transform wallCheck;
     [SerializeField] private float groundCheckRadius = 0.2f;
+    [SerializeField] private Vector2 wallCheckSize;
     [SerializeField] private LayerMask groundLayer;
     [SerializeField] private bool isGrounded;
+    [SerializeField] private bool isTouchingWall;
+
+    [Header("Dash Settings")] [SerializeField]
+    private float dashVelocity = 15f;
+
+    [SerializeField] private float dashTime = 0.1f;
+    private Vector2 _dashDirection;
+    private bool _isDashing;
+    private bool _canDash = true;
 
     private void Awake()
     {
-        rb = GetComponent<Rigidbody2D>();
+        _rb = GetComponent<Rigidbody2D>();
     }
 
     private void Update()
     {
-        currentSpeed = GetCurrentSpeed();
-        if (currentSpeed != 0)
-        {
-            Debug.Log("Current Speed: " + currentSpeed);
-        }
-
-        moveInput = Input.GetAxisRaw("Horizontal");
-        
+        _moveInput = Input.GetAxisRaw("Horizontal");
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+        isTouchingWall = Physics2D.OverlapBox(wallCheck.position, wallCheckSize, 0, groundLayer);
 
         if (Input.GetKeyDown(KeyCode.Period) && isGrounded)
         {
             Jump();
         }
+
+        if (Input.GetButtonDown("Dash") && _canDash)
+        {
+            StartDash();
+        }
+
+        if (isGrounded && !_isDashing)
+        {
+            _canDash = true;
+        }
     }
 
     private void FixedUpdate()
     {
-        Run();
-        SetJumpFallSpeed();
-    }
-
-    void Jump()
-    {
-        rb.velocity = new Vector2(rb.velocity.x, jumpVelocity);
-    }
-
-    void SetJumpFallSpeed()
-    {
-        if (rb.velocity.y < 0)
+        if (!_isDashing)
         {
-            rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.fixedDeltaTime;
-        }
-        else if (rb.velocity.y > 0 && !(Input.GetKey(KeyCode.Period)))
-        {
-            rb.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.fixedDeltaTime;
+            Run();
+            SetJumpFallSpeed();
         }
     }
 
-    void Run()
+    private void StartDash()
     {
-        if (moveInput != 0)
+        _isDashing = true;
+        _canDash = false;
+
+        _dashDirection = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        if (_dashDirection == Vector2.zero)
         {
-            rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
+            _dashDirection = new Vector2(transform.localScale.x, 0); // Default to horizontal dash
+        }
+
+        _dashDirection.Normalize();
+        _rb.velocity = _dashDirection * dashVelocity;
+
+        StartCoroutine(StopDash());
+    }
+
+    private IEnumerator StopDash()
+    {
+        yield return new WaitForSeconds(dashTime);
+        _isDashing = false;
+
+        _rb.velocity = new Vector2(0, _rb.velocity.y);
+    }
+
+    private void Jump()
+    {
+        _rb.velocity = new Vector2(_rb.velocity.x, jumpVelocity);
+    }
+
+    private void SetJumpFallSpeed()
+    {
+        if (_rb.velocity.y < 0)
+        {
+            _rb.velocity += Vector2.up * (( Physics2D.gravity.y * (fallMultiplier - 1)) * Time.fixedDeltaTime);
+        }
+        else if (_rb.velocity.y > 0 && !(Input.GetKey(KeyCode.Period)))
+        {
+            _rb.velocity += Vector2.up * ((Physics2D.gravity.y * (lowJumpMultiplier - 1)) * Time.fixedDeltaTime);
+
+        }
+    }
+
+    private void Run()
+    {
+        if (_moveInput != 0)
+        {
+            _rb.velocity = new Vector2(_moveInput * moveSpeed, _rb.velocity.y);
         }
         else
         {
-            rb.velocity = new Vector2(0, rb.velocity.y);
+            _rb.velocity = new Vector2(0, _rb.velocity.y);
         }
-    }
-
-    float GetCurrentSpeed()
-    {
-        return rb.velocity.x;
     }
 
     private void OnDrawGizmos()
     {
+        if (wallCheck == null) return;
+        {
+            Gizmos.color = Color.green;
+            Gizmos.DrawWireCube(wallCheck.position, wallCheckSize);
+        }
         if (groundCheck == null) return;
-        Gizmos.color = Color.blue;
+        Gizmos.color = Color.green;
         Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
     }
 }
